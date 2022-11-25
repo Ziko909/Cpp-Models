f480b2dfd1bd9d6c12358dfd936a8092937ece41
Ziko909 <zikoziko078@gmail.com>
2022-11-25 06:34:59 +0100

    chore : Add functions Prototype ✨

---
+++ Module 00/ex01/include/Include.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   include.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaabou <zaabou@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/25 06:24:04 by zaabou            #+#    #+#             */
+/*   Updated: 2022/11/25 06:27:02 by zaabou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INCLUDE_H
+# define INCLUDE_H
+
+# include <PhoneBook.hpp>
+# include <Contact.hpp>
+// get input from the user
+void	get_input(std::string& element, int mode);
+#endif
