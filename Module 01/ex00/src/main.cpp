d2633c19cd1a7661810c47759cf1357deec5ae18
Ziko909 <zikoziko078@gmail.com>
2022-12-03 21:42:21 +0100

    perf : protect the memory allocation 🚀

--- Module 01/ex00/src/main.cpp
+++ Module 01/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zaabou <zaabou@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 18:19:56 by zaabou            #+#    #+#             */
-/*   Updated: 2022/11/29 04:05:28 by zaabou           ###   ########.fr       */
+/*   Updated: 2022/12/03 21:41:29 by zaabou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,16 @@ int	main()
 	Zombie	*new_zombie;
 
 	new_zombie = newZombie("one");
-	new_zombie->announce();
-	randomChump("two");
-	delete new_zombie;
-	return (0);
+	if (new_zombie == NULL)
+	{
+		std::cerr << "Memory Allocation Failed" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	else
+	{
+		new_zombie->announce();
+		randomChump("two");
+		delete new_zombie;
+	}
+	return (EXIT_SUCCESS);
 }
