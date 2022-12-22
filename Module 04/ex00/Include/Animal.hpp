37765f124c75c974c026d4fde13b541315cf3271
Ziko909 <zikoziko078@gmail.com>
2022-12-22 08:47:23 +0100

    feat : implement Animal Class ✨

---
+++ Module 04/ex00/Include/Animal.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaabou <zaabou@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/21 12:47:27 by zaabou            #+#    #+#             */
+/*   Updated: 2022/12/22 08:33:38 by zaabou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+#define ANIMAL_HPP
+
+#include <iostream>
+
+class Animal
+{
+	protected :
+		std::string	mType;
+	public :
+						Animal();
+						Animal( std::string type );
+						Animal( const Animal& obj );
+		Animal&			operator = ( const Animal& rhs );
+		virtual			~Animal();
+		virtual	void	makeSound(  void ) const;
+		std::string		getType( void ) const;
+};
+
+#endif
