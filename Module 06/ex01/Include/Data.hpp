4b6d1b355af5469731265344d476d378512e6709
Ziko909 <zikoziko078@gmail.com>
2022-12-31 01:21:28 +0100

    feat : implement  Data Struct ✨

---
+++ Module 06/ex01/Include/Data.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zaabou <zaabou@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/29 18:24:27 by zaabou            #+#    #+#             */
+/*   Updated: 2022/12/29 19:48:05 by zaabou           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	DATA_HPP
+#define	DATA_HPP
+
+#include <cstdint>
+#include <iostream>
+
+typedef	struct Data
+{
+	char	character;
+	int		number;
+}t_Data;
+
+
+#endif
